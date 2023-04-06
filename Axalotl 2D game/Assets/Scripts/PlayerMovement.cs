d36fc6e0282863d65fdd7e1b0a2b9f744b5d45d0
AnnaPast <115112880+AnnaPast@users.noreply.github.com>
2023-04-06 12:33:54 +0200

    player movement

--- Axalotl 2D game/Assets/Scripts/PlayerMovement.cs
+++ Axalotl 2D game/Assets/Scripts/PlayerMovement.cs
@@ -5,18 +5,37 @@ public class PlayerMovement : MonoBehaviour
 {
     [SerializeField] private float speed;
     private Rigidbody2D body;
+    private bool grounded;
 
     private void Awake()
     {
+        //grab references from object
         body = GetComponent<Rigidbody2D>();
     }
 
     private void Update()
     {
-        body.velocity = new Vector2(Input.GetAxis("Horizontal") * speed, body.velocity.y);
+        float horizontalInput = Input.GetAxis("Horizontal");
 
-        if (Input.GetKey(KeyCode.Space)){
-            body.velocity = new Vector2(body.velocity.x, speed);
+        body.velocity = new Vector2(horizontalInput * speed, body.velocity.y);
+
+    //flip player when moving left-right
+       if(horizontalInput>0.01f)
+            transform.localScale = Vector3.one;
+        else if(horizontalInput < -0.01f)
+            transform.localScale = new Vector3 (-1,1,1);
+
+       if (Input.GetKey(KeyCode.Space))
+       Jump();
+    }
+
+    private void Jump()
+    {   body.velocity = new Vector2(body.velocity.x, speed);
+        grounded = false;
+
         }
+    
+    private void OnCollisionEnter2D(Collision2D collision) {
+        
     }
 }
