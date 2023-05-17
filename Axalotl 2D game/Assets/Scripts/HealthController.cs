537d9ce78a330d5c147d951c31e029548a4ce4b2
SlarkOP <pimenodavid@gmail.com>
2023-05-17 16:07:57 +0300

    Respawn \ Checkpoint

--- Axalotl 2D game/Assets/Scripts/HealthController.cs
+++ Axalotl 2D game/Assets/Scripts/HealthController.cs
@@ -6,13 +6,17 @@ using UnityEngine.UI;
 public class HealthController : MonoBehaviour
 {
     public int playerHealth;
+    Vector2 checkpointPos;
 
     [SerializeField] private Image[] hearts;
+
 
     private void Start()
     {
         UpdateHealth();
+        checkpointPos = transform.position;
     }
+
 
     public void UpdateHealth()
     {
@@ -22,10 +26,36 @@ public class HealthController : MonoBehaviour
             {
                 hearts[i].color = Color.white;
             }    
-            else
+            else if (i > 0)
             {
                 hearts[i].color = Color.black;
             }
+            if (playerHealth == 0)
+            {
+                Die();
+            }
         }    
+        
     }
+
+
+    void Die()
+    {
+       Respawn();
+    }
+
+
+    public void UpdateCheckpoint(Vector2 pos)
+    {
+        checkpointPos = pos;
+    }
+
+
+    void Respawn()
+    {
+        transform.position = checkpointPos;
+        playerHealth = 3;
+    }
+
+
 }
