afc98ab31edc076b49915c5fedbf53428fa111d9
AnnaPast <115112880+AnnaPast@users.noreply.github.com>
2023-05-17 21:04:35 +0200

    Platform respawn

--- Axalotl 2D game/Assets/Scripts/FallingPlatform.cs
+++ Axalotl 2D game/Assets/Scripts/FallingPlatform.cs
@@ -5,24 +5,42 @@ using UnityEngine;
 public class FallingPlatform : MonoBehaviour
 {
     private float fallDelay = 1f;
+    private float respawnDelay = 10f;
     private float destroyDelay = 2f;
     public Animator animator;
+    private Vector2 initialPosition;
+    private bool isFalling = false;
 
     [SerializeField] private Rigidbody2D rb;
 
+    private void Start()
+    {
+        initialPosition = transform.position;
+    }
+
     private void OnCollisionEnter2D(Collision2D collision)
     {
-        if (collision.gameObject.CompareTag("Player"))
+        if (collision.gameObject.CompareTag("Player") && !isFalling)
         {
             StartCoroutine(Fall());
-            animator.SetBool("shaking", true);
         }
     }
 
     private IEnumerator Fall()
     {
+        isFalling = true;
+        animator.SetBool("shaking", true);
+
         yield return new WaitForSeconds(fallDelay);
         rb.bodyType = RigidbodyType2D.Dynamic;
-        Destroy(gameObject, destroyDelay);
+
+        yield return new WaitForSeconds(destroyDelay);
+        rb.bodyType = RigidbodyType2D.Static;
+        rb.velocity = Vector2.zero;
+        transform.position = initialPosition;
+
+        yield return new WaitForSeconds(respawnDelay);
+        animator.SetBool("shaking", false);
+        isFalling = false;
     }
 }
