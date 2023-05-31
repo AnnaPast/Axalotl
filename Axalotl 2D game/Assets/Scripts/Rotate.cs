b47c4ad05b77a2b3f60e8d10cc6ada943fb5c9e8
SlarkOP <pimenodavid@gmail.com>
2023-05-31 22:32:37 +0300

    strashno

--- Axalotl 2D game/Assets/Scripts/Rotate.cs
+++ Axalotl 2D game/Assets/Scripts/Rotate.cs
@@ -3,13 +3,41 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class Rotate : MonoBehaviour
-{
-    public float RotateSpeed;
 
-    
+{   [SerializeField] private int aiDamage;
+    public float RotateSpeed;
+    [SerializeField] private HealthController _healthController;
+    public PlayerMovement playerMovement;
+
     void FixedUpdate()
     {
         float angle = transform.eulerAngles.z;
         transform.Rotate(0, 0, RotateSpeed * 1f * Time.deltaTime);
     }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+
+
+        if (collision.gameObject.tag == "Player")
+        {
+            playerMovement.KBCounter = playerMovement.KBTotalTime;
+            if (collision.transform.position.x <= transform.position.x)
+            {
+                playerMovement.KnockFromRight = true;
+            }
+            if (collision.transform.position.x > transform.position.x)
+            {
+                playerMovement.KnockFromRight = false;
+            }
+            Damage();
+        }
+    }
+
+    void Damage()
+    {
+        _healthController.playerHealth = _healthController.playerHealth - aiDamage;
+        _healthController.UpdateHealth();
+    }
+
 }
